01fd2ae01888457feea4c7c587ca82b0f1cd953f
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-12-07 18:07:25 +0530

    socket

---
+++ class/SocketObject.js
@@ -0,0 +1,37 @@
+import { uniqueNamesGenerator, colors, animals } from "unique-names-generator";
+
+const customConfig = {
+  dictionaries: [colors, animals],
+  separator: " ",
+  length: 2,
+  style: "capital",
+};
+
+class SocketObject {
+  constructor(id, room) {
+    this.id = id;
+    this.room = room;
+    this.name = uniqueNamesGenerator(customConfig);
+    this.image = `https://avatars.dicebear.com/api/avataaars/${this.name.replace(
+      " ",
+      ""
+    )}.svg?mood[]=happy`;
+  }
+
+  getId() {
+    return this.id;
+  }
+  getRoom() {
+    return this.room;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getImage() {
+    return this.image;
+  }
+}
+
+export default SocketObject;
